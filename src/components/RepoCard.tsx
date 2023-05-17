03f13b2f70b063f7f16a8f84a34a907acc61860f
Derek Wan <d.wan@icloud.com>
2023-05-17 20:53:52 +0800

    Can unsave repos

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -17,11 +17,19 @@ interface Props {
 }
 
 const saveRepo = (repo: Repo) => {
-  setSavedRepos([...savedRepos(), repo]);
-  // const current = savedRepos();
-  // if (!current.includes(repo)) {
-  // 	setSavedRepos([...current, repo]);
-  // }
+  // setSavedRepos([...savedRepos(), repo]);
+  const current = savedRepos();
+  if (!current.includes(repo)) {
+    setSavedRepos([...current, repo]);
+  }
+};
+
+const unsaveRepo = (repo: Repo) => {
+  setSavedRepos(savedRepos()?.filter((r) => r.id !== repo.id));
+};
+
+const repoIsSaved = (repo: Repo) => {
+  return savedRepos()?.some((r) => r.id === repo.id);
 };
 
 const RepoCard: Component<Props> = ({ repo }) => {
@@ -38,9 +46,15 @@ const RepoCard: Component<Props> = ({ repo }) => {
           <strong>{repo.owner?.login}</strong>/{repo.name}
         </a>
         <p class="card-text">{repo.description}</p>
-        <button class="btn btn-success" onClick={() => saveRepo(repo)}>
-          Save
-        </button>
+        {repoIsSaved(repo) ? (
+          <button class="btn btn-danger" onClick={() => unsaveRepo(repo)}>
+            Unsave
+          </button>
+        ) : (
+          <button class="btn btn-success" onClick={() => saveRepo(repo)}>
+            Save
+          </button>
+        )}
       </div>
     </div>
   );
