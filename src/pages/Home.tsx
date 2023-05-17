2d7e484ea268afe2c1d3911dd647e5bb8a813b7f
Derek Wan <d.wan@icloud.com>
2023-05-17 20:18:37 +0800

    Fetch data (#5)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
 import { Component } from "solid-js";
+import { repos, setUsername, username } from "../App";
 
 const Home: Component = () => {
+  const refetchWithUsername = (event: Event) => {
+    event.preventDefault();
+    const usernameInput = document.querySelector(
+      "#usernameInput",
+    ) as HTMLInputElement;
+    setUsername(usernameInput.value);
+  };
+
   return (
     <div>
-      <h2>Home</h2>
+      <form class="mb-3" onSubmit={refetchWithUsername}>
+        <input
+          type="text"
+          class="p-1 align-middle"
+          id="usernameInput"
+          required
+        />
+        <button class="btn btn-dark ms-3 w-auto">Fetch</button>
+      </form>
+      <h3>GitHub repos for {username()}</h3>
     </div>
   );
 };
